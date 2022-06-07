876ab26934557f1771f4345d0100b1bf233fd023
ElieBamunoba <eliebamunoba@gmail.com>
2022-06-07 16:25:07 +0300

    route with react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import {
+  Route,
+  Routes,
+  Switch,
+} from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites'
 
 
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path='/' element={<AllMeetupsPage />} exact>
+        </Route>
+        <Route path='/new-meetup' element={<NewMeetupPage />}>
+
+        </Route>
+        <Route path='/favorites' element={<FavoritesPage />}>
+
+        </Route>
+      </Routes>
+
+
+
     </div>
   );
 
 }
 
 export default App;
+
