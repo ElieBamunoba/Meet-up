c89a70f7dd3c679586ee45576cb5219d6e22d417
ElieBamunoba <eliebamunoba@gmail.com>
2022-06-07 14:42:24 +0300

    meet up app with react first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React from 'react';
-import Todo from './components/Todo';
 
 
 function App() {
   return (
     <div>
-      <h1> My Todos</h1>
-      <Todo text='learn React' />
-      <Todo text='learn Flutter' />
-      <Todo text='learn Django' />
-
-
-
-
     </div>
   );
 
