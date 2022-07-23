27c5e496953216e2b70bdc64faf57c49097a7a24
ElieBamunoba <eliebamunoba@gmail.com>
2022-07-23 20:09:34 +0300

    context

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -23,9 +23,6 @@ function AllMeetupsPage() {
                 }
                 meetups.push(meetup);
             }
-
-
-
             setIsLoading(false);
             setLoadedMeetups(meetups);
         });
